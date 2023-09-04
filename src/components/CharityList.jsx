b078da47c27ac9bc1c84e233237d29c17703fd5d
Nikola Bogicevic <nbogicevic2@gmail.com>
2023-09-04 10:43:04 -0400

    charity list component

--- src/components/CharityList.jsx
+++ src/components/CharityList.jsx
@@ -1,1 +1,15 @@
+import React from "react";
 
+function CharityList({ charities, onItemClick }) {
+	return (
+		<ul>
+			{charities.map((charity) => (
+				<li key={charity.id} onClick={() => onItemClick(charity.id)}>
+					{charity.name}
+				</li>
+			))}
+		</ul>
+	);
+}
+
+export default CharityList;
